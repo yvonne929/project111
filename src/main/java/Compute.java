99264add1fb0dae465394bbbb52fcabbb775e14a
yvonne <yvonne20030929@gmail.com>
2022-09-16 18:16:30 +0800

    Compute

---
+++ src/main/java/Compute.java
@@ -0,0 +1,10 @@
+public class Compute {
+    public static void main(String[] args) {
+        int x ;
+        x = 5;
+        x = 10;
+        x = x+1;
+        x += 1;
+        System.out.println(x);
+    }
+}
