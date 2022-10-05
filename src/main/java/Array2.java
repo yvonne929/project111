649128b247e597c7bb74cbdff0a500f7c5c6897e
yvonne929 <yvonne20030929@gmail.com>
2022-10-05 10:33:43 +0800

    Array2

---
+++ src/main/java/Array2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Array2 {
+    public static void main(String[] args) {
+        ArrayList<String> manyHeroes = new ArrayList<>();
+        manyHeroes.add("Iron Man");
+        manyHeroes.add("Ant Man");
+        manyHeroes.add("Spider Man");
+        System.out.println(manyHeroes.size());
+        System.out.println(manyHeroes.get(2));//取得陣列中 index = 2 的物件
+
+        Collections.sort(manyHeroes);//按照英文字母排序A~Z
+        System.out.println(manyHeroes);
+
+        manyHeroes.clear();//清除所有物件
+        System.out.println(manyHeroes.size());
+    }
+}
