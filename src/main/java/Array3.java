65e0f71c8ff1183606597d1b84e19bd23c1c9a16
yvonne929 <yvonne20030929@gmail.com>
2022-10-05 10:40:20 +0800

    Array3

---
+++ src/main/java/Array3.java
@@ -0,0 +1,10 @@
+public class Array3 {
+    public static void main(String[] args) {
+        int[][] my2DArray = new int[3][4];
+        my2DArray[0][0] = 90;
+        my2DArray[0][1] = 66;
+        my2DArray[0][2] = 79;
+        my2DArray[0][3] = 84;
+        my2DArray[1][0] = 76;
+    }
+}
