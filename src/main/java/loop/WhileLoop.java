ba333f2ab18ae496380e4c85c48afd0df03e465f
yvonne929 <yvonne20030929@gmail.com>
2022-10-05 11:17:20 +0800

    whileLoop

---
+++ src/main/java/loop/WhileLoop.java
@@ -0,0 +1,22 @@
+package loop;
+
+/*
+while迴圈，()裡為true時會執行
+ */
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        /*
+        while(true){//無窮迴圈
+            System.out.println("hi");
+        }
+         */
+
+        int i = 0;
+        while (i<10){
+            System.out.println("This is " + i);
+            i++;
+        }
+    }
+}
