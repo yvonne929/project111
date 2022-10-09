74eac1f58b6702e82a501354f86e982f4e506a9f
語萱 簡 <410570011@m365.fju.edu.tw>
2022-10-09 15:48:25 +0800

    GuessNumber 解決bug 修改

--- src/main/java/GuessNumber.java
+++ src/main/java/GuessNumber.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); //使用者輸入
-        int min = 0;    //宣告固定的最小值
-        int MAX = 100;  //宣告固定的最大值
+        int min = 0;    //宣告最小值
+        int MAX = 100;  //宣告最大值
 
         Random random = new Random(); //隨機產生亂數
         int ansNum = random.nextInt(MAX-min)+min; //產生min~(MAX-min)的數字
@@ -15,14 +15,14 @@ public class GuessNumber {
             System.out.println("請猜一個數字(介於" + min + "到" + MAX +")");
             int guessNum = scanner.nextInt(); //使用者輸入要猜的數字，原為String，利用.nextInt()轉成int
 
-            if(guessNum == ansNum){
-                System.out.println("你猜到!答案是"+ansNum);
-                break;
+            if(guessNum == ansNum){ //猜的數字和答案一樣
+                System.out.println("你猜到!答案是"+ansNum);// 印出答對及答案
+                break;  //跳出 while迴圈，程式停止
             }
 
             //使用雙層if-else，來阻止bug發生
             else if (guessNum < ansNum){   //當猜的數字小於答案
-                if (guessNum > min){       //猜的數字要大於最小值
+                if (guessNum > min){       //猜的數字要大於 min
                     min = guessNum;        //才會執行此行
                 }
                 else {
@@ -30,7 +30,7 @@ public class GuessNumber {
                 }
             }
             else if(guessNum > ansNum){   //當猜的數字大於答案
-                if (guessNum < MAX) {     //猜的數字要小於最大值
+                if (guessNum < MAX) {     //猜的數字要小於 MAX
                     MAX = guessNum;       //才會執行此行
                 }
                 else {
