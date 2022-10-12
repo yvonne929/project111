19d1f22ea8990072e212aff33e4a4a010d1b3c88
yvonne929 <yvonne20030929@gmail.com>
2022-10-12 09:45:19 +0800

    GuessNumber 修改2

--- src/main/java/GuessNumber.java
+++ src/main/java/GuessNumber.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); //使用者輸入
-        int min = 0;    //宣告最小值
-        int MAX = 100;  //宣告最大值
+        int min = 0;
+        int MAX = 100;
 
         Random random = new Random(); //隨機產生亂數
         int ansNum = random.nextInt(MAX-min)+min; //產生min~(MAX-min)的數字
-        System.out.println("answer is "+ansNum); //先印出ansNum
+        System.out.println("answer is "+ansNum);
 
         while (true){
             System.out.println("請猜一個數字(介於" + min + "到" + MAX +")");
