5d32c66af2d36cbd0f9a68e3f57ad1d86940738b
語萱 簡 <410570011@m365.fju.edu.tw>
2022-10-15 15:41:51 +0800

    GUI 放圖片

--- src/main/java/ch3/GUI.java
+++ src/main/java/ch3/GUI.java
@@ -5,13 +5,19 @@ import java.awt.*;
 
 public class GUI extends JPanel {
     public void paintComponent(Graphics g){
+        //在 Panel裡設文字
         //g.drawString("Hello world in window",150,150);
-        g.setColor(Color.BLUE);
-        g.drawLine(100,80,355,290);
-        g.setColor(Color.RED);
-        g.drawRect(50,50,120,80);
-        g.setColor(Color.orange);
-        g.fillRect(50,50,120,80);
+
+        //在 Panel設線條及圖形
+//        g.setColor(Color.BLUE);
+//        g.drawLine(100,80,355,290);
+//        g.setColor(Color.RED);
+//        g.drawRect(50,50,120,80);
+//        g.setColor(Color.orange);
+//        g.fillRect(50,50,120,80);
+
+        ImageIcon img = new ImageIcon("lantern.jpg");
+        img.paintIcon(null,g,20,20);
     }
 
     public static void main(String[] args) {
