319f3a84e102bc6438bae22d8524e1d30da77422
yvonne929 <yvonne20030929@gmail.com>
2022-10-19 11:36:35 +0800

    輸出所有水仙花數 修改

--- src/main/java/ch4/NumberOfDaffodils.java
+++ src/main/java/ch4/NumberOfDaffodils.java
@@ -5,15 +5,15 @@ package ch4;
 
 public class NumberOfDaffodils {
     public static void main(String[] args) {
-        for (int i = 100 ; i < 1000 ; i++) {
-        int units = i%10;
-        int  tens= (i/10)%10;
-        int hundreds = (i/100)%10;
+        for (int i = 100; i < 1000; i++) {
+            int units = i % 10;
+            int tens = (i / 10) % 10;
+            int hundreds = (i / 100) % 10;
 
-        int sum = (int)(Math.pow(units,3)+Math.pow(tens,3)+Math.pow(hundreds,3));
-        if (sum == i){
-            System.out.println(i);
-        }
+            int sum = (int) (Math.pow(units, 3) + Math.pow(tens, 3) + Math.pow(hundreds, 3));
+            if (sum == i) {
+                System.out.println(i);
+            }
         }
     }
 }
