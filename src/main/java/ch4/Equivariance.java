dd94a7c3aecb1fb42cbc5556583117b959d62ec7
yvonne929 <yvonne20030929@gmail.com>
2022-10-19 09:33:11 +0800

    等差數列 公差3

---
+++ src/main/java/ch4/Equivariance.java
@@ -0,0 +1,13 @@
+package ch4;
+
+public class Equivariance {
+    public static void printEvery3(){
+        for (int i = 1; i <= 88; i+=3) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printEvery3();
+    }
+}
