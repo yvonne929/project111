d1d16366b9af88056d52d037ab63284dd3d9a1f2
yvonne929 <yvonne20030929@gmail.com>
2022-10-19 11:35:48 +0800

    輸出所有水仙花數

---
+++ src/main/java/ch4/NumberOfDaffodils.java
@@ -0,0 +1,19 @@
+package ch4;
+
+//輸出所有的「水仙花數」
+//水仙花數: 153 = 1^3+5^3+3^3
+
+public class NumberOfDaffodils {
+    public static void main(String[] args) {
+        for (int i = 100 ; i < 1000 ; i++) {
+        int units = i%10;
+        int  tens= (i/10)%10;
+        int hundreds = (i/100)%10;
+
+        int sum = (int)(Math.pow(units,3)+Math.pow(tens,3)+Math.pow(hundreds,3));
+        if (sum == i){
+            System.out.println(i);
+        }
+        }
+    }
+}
