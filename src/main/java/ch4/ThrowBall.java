03501e959dcaedd286b4b698b03d46267e97a7ef
yvonne929 <yvonne20030929@gmail.com>
2022-10-19 10:55:17 +0800

    球落下彈回的距離

---
+++ src/main/java/ch4/ThrowBall.java
@@ -0,0 +1,22 @@
+package ch4;
+
+import java.util.Scanner;
+
+//一球從h米高度自由落下，每次落地後反跳回原高度的一半，再落下。求小球在第n次落地時，共經過多少公尺？
+
+public class ThrowBall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input the height: ");
+        int h = scanner.nextInt();
+        System.out.print("Input the num: ");
+        int n = scanner.nextInt();
+
+        double result = h;
+        for (int i = 2; i <= n ; i++) {
+            result+= 2*(h/Math.pow(2,i-1));
+        }
+
+        System.out.println("The distance is " + result);
+    }
+}
