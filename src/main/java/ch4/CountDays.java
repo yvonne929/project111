d4986a818855ee5635137d884aa470ec36b7b158
語萱 簡 <410570011@m365.fju.edu.tw>
2022-10-22 16:02:44 +0800

    CountDays 修改

--- src/main/java/ch4/CountDays.java
+++ src/main/java/ch4/CountDays.java
@@ -9,15 +9,22 @@ public class CountDays {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input year: ");
         int year = scanner.nextInt();
-        System.out.println("Input mouth: ");
+        System.out.print("Input mouth: ");
         int mouth = scanner.nextInt();
-        System.out.println("Input day: ");
+        System.out.print("Input day: ");
         int day = scanner.nextInt();
 
         int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
         int result = 0;
         for (int i = 0; i < mouth-1; i++) {
-            result+=days[i];
+            //依提示得知:閏年為4的倍數"且"不為100的倍數，"或"為400的倍數
+            if ((year%4 == 0 && year%100 != 0) || (year%400 == 0)){
+                days[1] = 29; //if內成立，days[1] (即二月天數) 改為29天
+                result+=days[i];
+            }
+            else { //若非，則用原本 days[1] = 28天計算
+                result+=days[i];
+            }
         }
         result+=day;
         System.out.println(result);
