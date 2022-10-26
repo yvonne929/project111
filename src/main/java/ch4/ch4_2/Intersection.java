4bd7d18a67a0c81686a28b8c9f679004cf314129
yvonne929 <yvonne20030929@gmail.com>
2022-10-26 11:19:02 +0800

    intersection

---
+++ src/main/java/ch4/ch4_2/Intersection.java
@@ -0,0 +1,32 @@
+package ch4.ch4_2;
+
+//找出兩陣列共同的值
+
+import ch2.array.Array;
+
+import java.util.ArrayList;
+
+public class Intersection {
+    public static int[] intersection(int[] arr1,int[] arr2){
+        ArrayList<Integer> outputs = new ArrayList<>();
+        for (int i : arr1) {
+            for (int j : arr2) {
+                if (i == j){
+                    outputs.add(i);
+                }
+            }
+        }
+        int[] result = new int[outputs.size()];
+        for (int k = 0; k < result.length; k++) {
+            result[k] = outputs.get(k);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] intersect = intersection(new int[] {1,3,4,6,10},new int[] {5,11,4,3,9,1});
+        for (int i : intersect) {
+            System.out.print(i + " ");
+        }
+    }
+}
