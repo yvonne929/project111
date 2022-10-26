c78fd4ddd2420183831d28ccd9f2890a22fa70ee
yvonne929 <yvonne20030929@gmail.com>
2022-10-26 10:40:44 +0800

    pyramid

---
+++ src/main/java/ch4/ch4_2/Pyramid.java
@@ -0,0 +1,20 @@
+package ch4.ch4_2;
+
+public class Pyramid {
+    public static void pyramid(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pyramid(1);
+        System.out.println("==========");
+        pyramid(3);
+        System.out.println("==========");
+        pyramid(5);
+    }
+}
