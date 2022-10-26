c7e063fd7787aa416a8dd75d63e61a334914410a
yvonne929 <yvonne20030929@gmail.com>
2022-10-26 09:39:21 +0800

    findMax

--- src/main/java/ch4/FindMax.java
+++ src/main/java/ch4/FindMax.java
@@ -2,7 +2,7 @@ package ch4;
 
 public class FindMax {
     public static int findMax(int[] inputs){
-        int Max = 4; // 初始值不設0，因位有可能全部都比0小，所以先預設第一個為Max
+        int Max = 4; // 初始值不設0，因為有可能全部都比0小，所以先預設第一個為Max
         for (int i = 1; i < inputs.length ; i++) {
             if (inputs[i] > Max){
                 Max = inputs[i];
