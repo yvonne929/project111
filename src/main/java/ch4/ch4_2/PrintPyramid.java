06a97aa606475f429da9bd5ec3eab8dd21fe66f4
語萱 簡 <410570011@m365.fju.edu.tw>
2022-11-01 19:10:54 +0800

    printPyramid 理解完成

--- src/main/java/ch4/ch4_2/PrintPyramid.java
+++ src/main/java/ch4/ch4_2/PrintPyramid.java
@@ -3,22 +3,22 @@ package ch4.ch4_2;
 public class PrintPyramid {
     public static void printStars(int space,int stars){
         String outStr = "";
-        for (int i = 0; i < space; i++) {
+        for (int i = 0; i < space; i++) { //印出??個空白
             outStr += " ";
         }
-        for (int i = 0; i < stars; i++) {
+        for (int i = 0; i < stars; i++) { //印出??個星星
             outStr += "*";
         }
         System.out.println(outStr);
     }
 
     public static void printPyramid(int n){
-        int stars = 1;
-        int space = n-1;
+        int space = n-1; //第一列有 n-1 個空白
+        int stars = 1; //一開始都是印 1 顆星
         while (space >= 0){
             printStars(space,stars);
-            space--;
-            stars+=2;
+            space--; //每次印完，space-1
+            stars+=2; //每次印完，stars多兩顆星(+2)
         }
     }
 
