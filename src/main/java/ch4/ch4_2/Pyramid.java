216e8158c3158284ac52d780c4d7f71b425c6b4c
語萱 簡 <410570011@m365.fju.edu.tw>
2022-11-01 19:56:49 +0800

    pyramid 修改

--- src/main/java/ch4/ch4_2/Pyramid.java
+++ src/main/java/ch4/ch4_2/Pyramid.java
@@ -1,19 +1,21 @@
 package ch4.ch4_2;
 
+//印出高度為n層的金字塔(直角三角形)
+
 public class Pyramid {
     public static void pyramid(int n){
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < i+1; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-        String output = "";
-        for (int i = 1; i <= n; i++) {
-            output+= "*";
-            System.out.print(output);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
+//        String output = "";
+//        for (int i = 1; i <= n; i++) {
+//            output+= "*";
+//            System.out.println(output);
+//        }
+//        System.out.println();
     }
 
     public static void main(String[] args) {
