520524b040c77b711d86c3d078faf97dd38e91c8
語萱 簡 <410570011@m365.fju.edu.tw>
2022-11-01 21:07:06 +0800

    CountDays 原程式碼和改位置

--- src/main/java/homework/CountDays.java
+++ src/main/java/homework/CountDays.java
@@ -1,4 +1,6 @@
-package ch4.ch4_1;
+package homework;
+
+//計算天數，輸入某年某月某日，讓程式判斷這一天是這一年的第幾天
 
 import java.util.Scanner;
 
@@ -15,7 +17,14 @@ public class CountDays {
         int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
         int result = 0;
         for (int i = 0; i < mouth-1; i++) {
-            result+= days[i];
+            //依提示得知:閏年為4的倍數"且"不為100的倍數，"或"為400的倍數
+            if ((year%4 == 0 && year%100 != 0) || (year%400 == 0)){
+                days[1] = 29; //if內成立，days[1] (即二月天數) 改為29天
+                result+=days[i];
+            }
+            else { //若非，則用原本 days[1] = 28天計算
+                result+=days[i];
+            }
         }
         result+=day;
         System.out.println(result);
