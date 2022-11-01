dd370ea402c184eea47fd7ab4a61a5af5e1e4b95
語萱 簡 <410570011@m365.fju.edu.tw>
2022-11-01 20:36:45 +0800

    printEvery 修改名字

--- src/main/java/ch4/ch4_1/PrintEvery.java
+++ src/main/java/ch4/ch4_1/PrintEvery.java
@@ -3,7 +3,7 @@ package ch4.ch4_1;
 //等差數列 公差3
 //1,4,7,...,88
 
-public class ArithmeticProgression {
+public class PrintEvery {
     public static void printEvery3() {
         for (int i = 1; i <= 88; i += 3) {
             System.out.println(i);
