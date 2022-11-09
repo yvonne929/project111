baa9b42adec23f6bae81dfd92b3d7ae2b573b3f4
yvonne929 <yvonne20030929@gmail.com>
2022-11-09 11:26:32 +0800

    circle overload

--- src/main/java/ch5/Circle.java
+++ src/main/java/ch5/Circle.java
@@ -1,10 +1,17 @@
 package ch5;
 
+/*
+private 只能在當前的 class 裡使用
+public 大家都可以用
+ */
 public class Circle {
     public double radius;
 
-    public Circle(double newRadius){ //名字要和class一樣(Constructor of Circle class)
+    public Circle(double newRadius) { //名字要和class一樣(Constructor of Circle class)
         radius = newRadius;
+    }
 
+    public Circle() {
+        radius = 1.0;
     }
 }
--- src/main/java/ch5/Main.java
+++ src/main/java/ch5/Main.java
@@ -2,9 +2,13 @@ package ch5;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(10);
+        Circle circle = new Circle(10);//有輸入值
         System.out.println(circle.radius);
-        Circle c2 = new Circle(3);
+
+        Circle c2 = new Circle(3);//有輸入值
         System.out.println(c2.radius);
+
+        Circle c3 = new Circle();//無輸入值
+        System.out.println(c3.radius);
     }
 }
