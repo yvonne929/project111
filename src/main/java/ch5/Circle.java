bcf4ba2e0ab225d93e3b84c7dc00397b5972608d
yvonne929 <yvonne20030929@gmail.com>
2022-11-09 11:04:38 +0800

    circle

---
+++ src/main/java/ch5/Circle.java
@@ -0,0 +1,10 @@
+package ch5;
+
+public class Circle {
+    public double radius;
+
+    public Circle(double newRadius){ //名字要和class一樣(Constructor of Circle class)
+        radius = newRadius;
+
+    }
+}
---
+++ src/main/java/ch5/Main.java
@@ -0,0 +1,10 @@
+package ch5;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        System.out.println(circle.radius);
+        Circle c2 = new Circle(3);
+        System.out.println(c2.radius);
+    }
+}
