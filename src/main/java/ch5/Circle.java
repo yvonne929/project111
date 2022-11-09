7eabd8c3a1dd8cd18dcd16e5870d0e8d1f51fee3
yvonne929 <yvonne20030929@gmail.com>
2022-11-09 11:49:08 +0800

    circle class scope

--- src/main/java/ch5/Circle.java
+++ src/main/java/ch5/Circle.java
@@ -3,11 +3,12 @@ package ch5;
 /*
 private 只能在當前的 class 裡使用
 public 大家都可以用
+scope 變數有效範圍
  */
 public class Circle {
-    public double radius;
+    public double radius;//為 A Java field，整個 class 都可以使用
 
-    public Circle(double newRadius) { //名字要和class一樣(Constructor of Circle class)
+    public Circle(double newRadius) { //名字要和 class 一樣(Constructor of Circle class)
         radius = newRadius;
     }
 
@@ -18,4 +19,14 @@ public class Circle {
     public double computeArea(){
         return Math.PI*radius*radius;
     }
+
+/*  i 的使用範圍只限於 setVar 裡 (區域變數)
+    public void setVar(){
+        int i = 100;
+    }
+
+    public void outputVar(){
+        System.out.println(i);
+    }
+ */
 }
