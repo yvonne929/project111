be384a8b6013cb3ae85b1a90635dbbb28c832737
yvonne929 <yvonne20030929@gmail.com>
2022-11-30 09:57:53 +0800

    加註解

--- src/main/java/ch5/Abstract/Animal.java
+++ src/main/java/ch5/Abstract/Animal.java
@@ -7,9 +7,11 @@ public abstract class Animal {
         this.animalName = animalName;
     }
 
+    //non-abstract methods (是object共同有的 methods)
     public void sleep(){
         System.out.println(animalName + " is sleeping.");
     }
 
+    //abstract methods (object可以在自己的class裡定義不同的動作)
     public abstract void makeSound();//不能直接用，只能到子類別裡建立
 }
