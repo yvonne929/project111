8298f7aaa1d2b91615217e4a8281de77dd36b5c4
yvonne929 <yvonne20030929@gmail.com>
2022-12-07 10:58:11 +0800

    sheep switch case

--- src/main/java/ch6/Main.java
+++ src/main/java/ch6/Main.java
@@ -39,19 +39,36 @@ public class Main extends JPanel implements KeyListener{
     @Override
     public void keyPressed(KeyEvent keyEvent) {//按下按鍵
         int k = keyEvent.getKeyCode();//取得按下按鍵的ASCII keyCode
-        System.out.println(k);
-        if (k == KeyEvent.VK_UP){//按"上"
-            s.walk(0,-5);
+        //System.out.println(k);
+        switch (k){//檢查 k
+            case KeyEvent.VK_UP://當 k 符合此條件
+                s.walk(0,-5);//做這件事
+                break;
+            case KeyEvent.VK_DOWN:
+                s.walk(0,5);
+                break;
+            case KeyEvent.VK_LEFT:
+                s.walk(-5,0);
+                break;
+            case KeyEvent.VK_RIGHT:
+                s.walk(5,0);
+                break;
+            default://按其他按鍵,顯示訊息視窗
+                JOptionPane.showMessageDialog(this,"請按方向鍵(Please input arrow key)");
         }
-        if (k == KeyEvent.VK_DOWN){//按"下"
-            s.walk(0,5);
-        }
-        if (k == KeyEvent.VK_LEFT){//按"左"
-            s.walk(-5,0);
-        }
-        if (k == KeyEvent.VK_RIGHT){//按"右"
-            s.walk(5,0);
-        }
+
+//        if (k == KeyEvent.VK_UP){//按"上"
+//            s.walk(0,-5);
+//        }
+//        if (k == KeyEvent.VK_DOWN){//按"下"
+//            s.walk(0,5);
+//        }
+//        if (k == KeyEvent.VK_LEFT){//按"左"
+//            s.walk(-5,0);
+//        }
+//        if (k == KeyEvent.VK_RIGHT){//按"右"
+//            s.walk(5,0);
+//        }
         repaint();//重新繪製
     }
 
