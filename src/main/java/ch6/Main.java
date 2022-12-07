d6fdc08061f94cb3d2aae84830be40336fab9804
yvonne929 <yvonne20030929@gmail.com>
2022-12-07 10:28:19 +0800

    sheep

--- src/main/java/ch6/Main.java
+++ src/main/java/ch6/Main.java
@@ -1,8 +1,62 @@
 package ch6;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Main extends JPanel implements KeyListener{
+    Sheep s;
+
+    //創建羊(物件)
+    public Main(){
+      s = new Sheep(40,40);
+      addKeyListener(this);
+    }
+
+    //繪製羊(物件)
+    @Override
+    public void paintComponent(Graphics g){
+        g.setColor(Color.BLACK);//設定視窗顏色
+        g.fillRect(0,0,getWidth(),getHeight());//顏色填滿整個視窗,意思:從(0,0)~(getWidth(),getHeight())
+        s.drawSheep(g);//用 s 裡的 method 繪製羊
+        requestFocusInWindow();
+    }
+
     public static void main(String[] args) {
-        Sheep s = new Sheep(10,20);
-        System.out.println(s);
+        JFrame window = new JFrame();
+        window.setContentPane(new Main());//視窗內容
+        window.setSize(500,500);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {//按下按鍵
+        int k = keyEvent.getKeyCode();//取得按下按鍵的ASCII keyCode
+        System.out.println(k);
+        if (k == 38){//按"上"
+            s.walk(0,-5);
+        }
+        if (k == 40){//按"下"
+            s.walk(0,5);
+        }
+        if (k == 37){//按"左"
+            s.walk(-5,0);
+        }
+        if (k == 39){//按"右"
+            s.walk(5,0);
+        }
+        repaint();//重新繪製
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
     }
 }
--- src/main/java/ch6/Sheep.java
+++ src/main/java/ch6/Sheep.java
@@ -1,5 +1,7 @@
 package ch6;
 
+import java.awt.*;
+
 public class Sheep {
     private int x;
     private int y;
@@ -9,8 +11,18 @@ public class Sheep {
         this.y = y;
     }
 
-    @Override
-    public String toString(){
-        return "The xy coordinate is " + x + "," + y;
+    public void drawSheep(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillOval(x+23,y+50,225,100);//羊的身體
+        g.fillOval(x,y,75,75);//羊的頭
+        g.fillRect(x+50,y+100,25,100);//羊的腳(1)
+        g.fillRect(x+100,y+100,25,100);//羊的腳(2)
+        g.fillRect(x+150,y+100,25,100);//羊的腳(3)
+        g.fillRect(x+200,y+100,25,100);//羊的腳(4)
+    }
+
+    public void walk(int x,int y){//更新座標,讓羊移動
+        this.x += x;
+        this.y += y;
     }
 }
