59b10d8ba9fc942e9ce01e6a8f5198ce6c5c87be
yvonne929 <yvonne20030929@gmail.com>
2022-12-14 11:36:39 +0800

    bubble sort 優化(查看執行過程有錯誤)

--- src/main/java/ch4/ch4_1/BubbleSort.java
+++ src/main/java/ch4/ch4_1/BubbleSort.java
@@ -11,23 +11,37 @@ package ch4.ch4_1;
 
 public class BubbleSort {
     public static void bubbleSort(int[] arr ){
+        int turn = 1; // 表示第??回合
         for (int i = 0; i < arr.length-1; i++) {
+            int earlyStop = 1;
             for (int j = 0; j < arr.length-1-i; j++) {
                 if (arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    earlyStop = 0;
                 }
             }
+            if (earlyStop == 1) break;
+
+            //查看執行過程
+            for (int k = 0; k < arr.length; k++) {
+                if (k == arr.length-1-turn){
+                    System.out.print(arr[k] + "[");
+                }
+                else if (k == arr.length-1) {
+                    System.out.print(arr[k] + "]");
+                }
+                else {
+                    System.out.print(arr[k] + " ");
+                }
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int[] array = {5,1,4,2,8};
-        for (int i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
+        int[] array = {6,5,4,3,2,1};
         bubbleSort(array);
         for (int i : array) {
             System.out.print(i + " ");
